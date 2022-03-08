0a168330b41817d630c8c43c286e6b0ab5f42605
mab <schoch1995@gmail.com>
2022-03-08 17:42:09 +0100

    added ability to set outputfunction in predictions

--- regression/predict.C
+++ regression/predict.C
@@ -4,11 +4,11 @@
 #include "TMVA/Reader.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TMVA/MethodDL.h"
 
 int predict(){
 
-    TMVA::Reader* reader = new TMVA::Reader("V:!Silent");
-    reader->PrintOptions();
+    TMVA::Reader* reader = new TMVA::Reader("!Silent");
     TFile* f = new TFile("mnist0.root");
     TTree* t = (TTree*)f->Get("test");
     Float_t x[784];
@@ -22,9 +22,12 @@ int predict(){
         reader->AddVariable(varName, &x[i]);
     }
     
-    reader->BookMVA("DL", "dataset/weights/MNIST_tmvaDL.weights.xml");
-    std::vector<Float_t> out = reader->EvaluateRegression("DL");
+    TMVA::IMethod* method = reader->BookMVA("DL", "dataset/weights/MNIST_tmvaDL.weights.xml");
+    // --- optional: set output function Softmax -> ArgMax or only ArgMax
+    // TMVA::MethodDL* mDL = dynamic_cast<TMVA::MethodDL*>(method);
+    // mDL->SetOutputFunction(TMVA::DNN::EOutputFunction::kSoftmax); 
     
+    std::vector<Float_t> out = reader->EvaluateRegression("DL");
     std::cout << "pred    " << "   true" << std::endl;
     for(int i{}; i < 10; i++){
         std::cout << out[i] << "   " << y[i] << std::endl;
