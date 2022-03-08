d8a4cd4173ace376659678dbaa5ba245c557c625
mab <schoch1995@gmail.com>
2022-03-08 13:24:27 +0100

    update

--- regression/train.py
+++ regression/train.py
@@ -6,7 +6,7 @@ from keras.layers import Dense, Reshape, Conv2D, MaxPooling2D, Flatten
 # Setup TMVA
 ROOT.TMVA.Tools.Instance()
 ROOT.TMVA.PyMethodBase.PyInitialize()
-ROOT.EnableImplicitMT(4)
+#ROOT.EnableImplicitMT(4)
 
 output = ROOT.TFile.Open("TMVA.root", "RECREATE")
 factory = ROOT.TMVA.Factory(
@@ -32,8 +32,8 @@ dataloader.PrepareTrainingAndTestTree(
 # Define model
 batchLayoutString = "BatchLayout=100|1|784:"
 inputLayoutString = "InputLayout=1|28|28:"
-layoutString = "Layout=CONV|4|3|3|1|1|1|1|RELU,MAXPOOL|2|2|1|1,RESHAPE|FLAT,DENSE|16|RELU,DENSE|10|IDENTITY:"
-trainingString = "TrainingStrategy=MaxEpochs=30,BatchSize=100,LearningRate=1e-3:"
+layoutString = "Layout=CONV|4|3|3|1|1|1|1|RELU,MAXPOOL|2|2|1|1,RESHAPE|FLAT,DENSE|16|RELU,DENSE|10|TANH:"
+trainingString = "TrainingStrategy=MaxEpochs=30,BatchSize=100,Optimizer=ADAM,LearningRate=1e-3:"
 cnnOptions = "H:V:VarTransform=None:ErrorStrategy=SUMOFSQUARES:VerbosityLevel=Debug:Architecture=GPU"
 options = batchLayoutString + inputLayoutString + layoutString + trainingString + cnnOptions
 
@@ -42,7 +42,17 @@ options = batchLayoutString + inputLayoutString + layoutString + trainingString 
 factory.BookMethod(dataloader, ROOT.TMVA.Types.kDL, "tmvaDL",
                    options)
 
+#factory.GetMethod(dataloader.GetName() ,"tmvaDL").SetOutputFunction(ROOT.TMVA.DNN.EOutputFunction.kSoftmax)
 # Run training, test and evaluation
 factory.TrainAllMethods()
 #factory.TestAllMethods()
 #factory.EvaluateAllMethods()
+
+# --------- load model from file
+""" factory1 = ROOT.TMVA.Factory(
+    "MNIST", 
+    "V:!Silent:Color:DrawProgressBar:!ROC:ModelPersistence:AnalysisType=Regression:Transformations=None:!Correlations:VerboseLevel=Debug")
+factory1.BookMethodWeightfile(dataloader, ROOT.TMVA.Types.kDL, "model.xml")
+model1 = factory1.GetMethod().GetDeepNet()
+pred
+prediction = Prediction( pred, , ) """
