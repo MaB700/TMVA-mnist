cf97cb49b158017b68cf28386150223ef1a16d21
mab <schoch1995@gmail.com>
2022-03-08 14:27:07 +0100

    cleanup

--- regression/train.py
+++ regression/train.py
@@ -1,8 +1,5 @@
 import ROOT
 
-from keras.models import Sequential
-from keras.layers import Dense, Reshape, Conv2D, MaxPooling2D, Flatten
-
 # Setup TMVA
 ROOT.TMVA.Tools.Instance()
 ROOT.TMVA.PyMethodBase.PyInitialize()
@@ -32,27 +29,20 @@ dataloader.PrepareTrainingAndTestTree(
 # Define model
 batchLayoutString = "BatchLayout=100|1|784:"
 inputLayoutString = "InputLayout=1|28|28:"
-layoutString = "Layout=CONV|4|3|3|1|1|1|1|RELU,MAXPOOL|2|2|1|1,RESHAPE|FLAT,DENSE|16|RELU,DENSE|10|TANH:"
+layoutString = "Layout=CONV|4|3|3|1|1|1|1|RELU,MAXPOOL|2|2|1|1,RESHAPE|FLAT,DENSE|16|RELU,DENSE|10|SIGMOID:"
 trainingString = "TrainingStrategy=MaxEpochs=30,BatchSize=100,Optimizer=ADAM,LearningRate=1e-3:"
 cnnOptions = "H:V:VarTransform=None:ErrorStrategy=SUMOFSQUARES:VerbosityLevel=Debug:Architecture=GPU"
 options = batchLayoutString + inputLayoutString + layoutString + trainingString + cnnOptions
 
 # Book methods
-
 factory.BookMethod(dataloader, ROOT.TMVA.Types.kDL, "tmvaDL",
                    options)
 
-#factory.GetMethod(dataloader.GetName() ,"tmvaDL").SetOutputFunction(ROOT.TMVA.DNN.EOutputFunction.kSoftmax)
+# Set output function -- TODO: using softmax yield worst results, may use different ErrorStrategy
+# factory.GetMethod(dataloader.GetName() ,"tmvaDL").SetOutputFunction(ROOT.TMVA.DNN.EOutputFunction.kSoftmax)
+
 # Run training, test and evaluation
 factory.TrainAllMethods()
 #factory.TestAllMethods()
 #factory.EvaluateAllMethods()
 
-# --------- load model from file
-""" factory1 = ROOT.TMVA.Factory(
-    "MNIST", 
-    "V:!Silent:Color:DrawProgressBar:!ROC:ModelPersistence:AnalysisType=Regression:Transformations=None:!Correlations:VerboseLevel=Debug")
-factory1.BookMethodWeightfile(dataloader, ROOT.TMVA.Types.kDL, "model.xml")
-model1 = factory1.GetMethod().GetDeepNet()
-pred
-prediction = Prediction( pred, , ) """
